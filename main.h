2ad4d98438b917cbaf73e47c05223a77e978535d
Mack Octavian <maestrooctavian@gmail.com>
2023-04-22 14:02:08 -0700

    Added a header file containing prototype functions

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: Header file containing prototypes for all functions
+ *       used in the printf project.
+ */
+
+
+int _printf(const char *format, ...);
+#endif
