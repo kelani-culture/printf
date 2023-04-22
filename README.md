7ef423f3ec01dcb43066908bdbc2cc2f6650f37d
Null <kelanidarasimi9@gmail.com>
2023-04-22 14:38:30 -0700

    fix error

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 printf project with marcus octavian
-=======
-  
->>>>>>> 2ad4d98438b917cbaf73e47c05223a77e978535d
--- main.h
+++ main.h
@@ -1,18 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-<<<<<<< HEAD
 int str_specifier(char *format);
 int _strlen(char *str);
 int _putchar(char chr);
-
-=======
-/**
- * Desc: Header file containing prototypes for all functions
- *       used in the printf project.
- */
-
-
 int _printf(const char *format, ...);
->>>>>>> 2ad4d98438b917cbaf73e47c05223a77e978535d
 #endif
